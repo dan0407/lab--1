32451ad15e5da7282c79e032a6b8bd9c27acb013
daniel <145810043+dan0407@users.noreply.github.com>
2024-02-15 01:37:41 -0500

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,4 @@
+const numeros = [110, 238, 32, 54, 7, 64, 70, 8, 98, 100];
+var imul = require('interval-multiply');
+console.log(imul([110, 238], [98, 70]));
+// escogi este paquete que multiplica valores de un array sin el paquete este no multiplicaria nada del contenido del array.
---
+++ data.js
@@ -0,0 +1,6 @@
+const frutas = ['banana', 'banana', 'pera', 'banana'];
+const dinero = 10000;
+module.exports = {
+	frutas: frutas,
+	dinero: dinero,
+};
